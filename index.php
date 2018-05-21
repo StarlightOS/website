859559950066a22d71d8fd91305a57fa941a4f3d
Érico Lopez <FriendlyHacker@users.noreply.github.com>
2018-05-20 22:18:15 -0300

    Only the custom changes to cloudbase3 template

---
+++ index.php
@@ -0,0 +1,14 @@
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@Starlight_Linux" />
+<meta name="twitter:title" content="Starlight Linux Operational System" />
+<meta name="twitter:description" content="The beautiful, gaming centric and easy to use Operational System. Download for free." />
+<meta name="twitter:creator" content="@Starlight_Linux">
+<meta name="twitter:image" content="https://www.starlightos.com/images/og-image.jpg" />
+<meta name="twitter:domain" content="www.starlightos.com" />
+<meta name="robots" content="follow,index" />
+<meta property='og:type' content="website" />
+<meta property='og:url' content="https://www.starlightos.com/" />
+<meta property='og:title' content="Starlight Linux Operational System" />
+<meta property='og:description' content="The beautiful, gaming centric and easy to use Operational System. Download for free." />
+<meta property='og:image' content="https://www.starlightos.com/images/og-image.jpg" />
+<link rel="author" href="https://www.facebook.com/Starlight-Linux-374531209726835/" />
